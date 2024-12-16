db430b5ba8c529925454398414e6140b0350e1dd
Android250 <android250@zamt.local>
2024-12-16 16:12:39 +0300

    Добавили компонент для OnBoardDescription

--- app/src/main/java/com/example/smartlab/components/OnBoardDescription.kt
+++ app/src/main/java/com/example/smartlab/components/OnBoardDescription.kt
@@ -1,2 +1,21 @@
 package com.example.smartlab.components
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.smartlab.ui.theme.textDescription
+
+@Composable
+fun OnBoardDescription (modifier: Modifier = Modifier,text:String) {
+    Text(
+        text = text,
+        color = textDescription,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp
+    )
+}
--- app/src/main/java/com/example/smartlab/ui/theme/Color.kt
+++ app/src/main/java/com/example/smartlab/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val textDescription = Color(0xFF939396)
+val onBoardHeader=Color(0xFF00B712)
+val textButton = Color(0xFF57A9FF)
